2792e5dd51051d76cd241b985cf0b5ba63c1d762
Dustin Sallings <dustin@spy.net>
2012-09-02 14:50:21 -0700

    Minor perf modification.
    There's not a lot to clean up here, but I tried a bunch of things and
    this is the only one that didn't actually make it slower.
    
    Before:  BenchmarkPaths	 2000000	      4206 ns/op
    After:   BenchmarkPaths	 2000000	      4138 ns/op

--- jsonpointer.go
+++ jsonpointer.go
@@ -9,14 +9,6 @@ import (
 	"strings"
 )
 
-func unescape(s string) string {
-	if strings.Contains(s, "~") {
-		s = strings.Replace(s, "~1", "/", -1)
-		s = strings.Replace(s, "~0", "~", -1)
-	}
-	return s
-}
-
 // Get the value at the specified path.
 func Get(m map[string]interface{}, path string) interface{} {
 	if path == "" {
@@ -27,9 +19,12 @@ func Get(m map[string]interface{}, path string) interface{} {
 	var rv interface{} = m
 
 	for _, p := range parts {
-		p = unescape(p)
 		switch v := rv.(type) {
 		case map[string]interface{}:
+			if strings.Contains(p, "~") {
+				p = strings.Replace(p, "~1", "/", -1)
+				p = strings.Replace(p, "~0", "~", -1)
+			}
 			rv = v[p]
 		case []interface{}:
 			i, err := strconv.Atoi(p)
