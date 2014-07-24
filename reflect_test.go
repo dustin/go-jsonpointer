d6e8668b1bba4e72c167f92cb7d42a7320710b14
Marty Schoch <marty.schoch@gmail.com>
2014-07-23 22:14:17 -0400

    improved reflection support
    The object being examined can now be a Map or an Array.
    Arrays should work identical to Slices and are not that interesting.
    Maps however are more interesting as keys can be a variety of types
    beyond that which JSON/JSONPointer support.
    This implementation allows numeric values (int/uint/float) to
    operate both when querying and when listing pointers.
    
    Consider the following exmaple:
    
    x := map[int]string{
      36: "marty"
    }
    
    y := Reflect(x, "/36")
    
    Here we would expect y to be the string "marty".

--- reflect_test.go
+++ reflect_test.go
@@ -17,6 +17,11 @@ type person struct {
 	Addresses          []*address `json:"addresses"`
 	NameTildeContained string     `json:"name~contained"`
 	NameSlashContained string     `json:"name/contained"`
+	AnActualArray      [4]int
+	NestedMap          map[string]float64 `json:"nestedmap"`
+	MapIntKey          map[int]string
+	MapUintKey         map[uint]string
+	MapFloatKey        map[float64]string
 }
 
 var input = &person{
@@ -34,6 +39,40 @@ var input = &person{
 	},
 	NameTildeContained: "yessir",
 	NameSlashContained: "nosir",
+	AnActualArray:      [4]int{0, 1, 2, 3},
+	NestedMap: map[string]float64{
+		"pi":          3.14,
+		"back/saidhe": 2.71,
+		"till~duh":    1.41,
+	},
+	MapIntKey: map[int]string{
+		1: "one",
+		2: "two",
+	},
+	MapUintKey: map[uint]string{
+		3: "three",
+		4: "four",
+	},
+	MapFloatKey: map[float64]string{
+		3.14: "pi",
+		4.15: "notpi",
+	},
+}
+
+func compareStringArrayIgnoringOrder(a, b []string) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	tmp := make(map[string]bool, len(a))
+	for _, av := range a {
+		tmp[av] = true
+	}
+	for _, bv := range b {
+		if tmp[bv] != true {
+			return false
+		}
+	}
+	return true
 }
 
 func TestReflectListPointers(t *testing.T) {
@@ -41,14 +80,40 @@ func TestReflectListPointers(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	expect := []string{"", "/name", "/Twitter", "/aliases", "/aliases/0", "/aliases/1", "/addresses", "/addresses/0", "/addresses/0/street", "/addresses/0/Zip", "/name~0contained", "/name~1contained"}
-	if !reflect.DeepEqual(pointers, expect) {
+	expect := []string{"", "/name", "/Twitter", "/aliases",
+		"/aliases/0", "/aliases/1", "/addresses", "/addresses/0",
+		"/addresses/0/street", "/addresses/0/Zip",
+		"/name~0contained", "/name~1contained", "/AnActualArray",
+		"/AnActualArray/0", "/AnActualArray/1", "/AnActualArray/2",
+		"/AnActualArray/3", "/nestedmap", "/nestedmap/pi",
+		"/nestedmap/back~1saidhe", "/nestedmap/till~0duh",
+		"/MapIntKey", "/MapIntKey/1", "/MapIntKey/2", "/MapUintKey",
+		"/MapUintKey/3", "/MapUintKey/4", "/MapFloatKey",
+		"/MapFloatKey/3.14", "/MapFloatKey/4.15"}
+	if !compareStringArrayIgnoringOrder(expect, pointers) {
 		t.Fatalf("expected %#v, got %#v", expect, pointers)
 	}
 }
 
 func TestReflectNonObjectOrSlice(t *testing.T) {
 	got := Reflect(36, "/test")
+	if got != nil {
+		t.Errorf("expected nil, got %#v", got)
+	}
+}
+
+type structThatCanBeUsedAsKey struct {
+	name   string
+	domain string
+}
+
+func TestReflectMapThatWontWork(t *testing.T) {
+
+	amapthatwontwork := map[structThatCanBeUsedAsKey]string{}
+	akey := structThatCanBeUsedAsKey{name: "marty", domain: "couchbase"}
+	amapthatwontwork[akey] = "verycontrived"
+
+	got := Reflect(amapthatwontwork, "/anykey")
 	if got != nil {
 		t.Errorf("expected nil, got %#v", got)
 	}
@@ -111,6 +176,38 @@ func TestReflect(t *testing.T) {
 		{
 			path: "/name~1contained",
 			exp:  "nosir",
+		},
+		{
+			path: "/AnActualArray/2",
+			exp:  2,
+		},
+		{
+			path: "/AnActualArray/5",
+			exp:  nil,
+		},
+		{
+			path: "/nestedmap/pi",
+			exp:  3.14,
+		},
+		{
+			path: "/nestedmap/back~1saidhe",
+			exp:  2.71,
+		},
+		{
+			path: "/nestedmap/till~0duh",
+			exp:  1.41,
+		},
+		{
+			path: "/MapIntKey/1",
+			exp:  "one",
+		},
+		{
+			path: "/MapUintKey/3",
+			exp:  "three",
+		},
+		{
+			path: "/MapFloatKey/3.14",
+			exp:  "pi",
 		},
 	}
 
