6812e303f1e1a32596be3328875231201149154f
Dustin Sallings <dustin@spy.net>
2016-08-05 18:54:49 -0700

    Separate the functions for listing and selecting pointers

--- ptrtool/ptrtool.go
+++ ptrtool/ptrtool.go
@@ -11,28 +11,37 @@ import (
 	"github.com/dustin/go-jsonpointer"
 )
 
+func listPointers(d []byte) {
+	l, err := jsonpointer.ListPointers(d)
+	if err != nil {
+		log.Fatalf("Error listing pointers: %v", err)
+	}
+	for _, p := range l {
+		fmt.Println(p)
+	}
+
+}
+
+func selectItems(d []byte, pointers []string) {
+	m, err := jsonpointer.FindMany(d, pointers)
+	if err != nil {
+		log.Fatalf("Error finding pointers: %v", err)
+	}
+	for k, v := range m {
+		b := &bytes.Buffer{}
+		json.Indent(b, v, "", "  ")
+		fmt.Printf("%v\n%s\n\n", k, b)
+	}
+}
+
 func main() {
 	d, err := ioutil.ReadAll(os.Stdin)
 	if err != nil {
 		log.Fatalf("Error reading json from stdin: %v", err)
 	}
 	if len(os.Args) == 1 {
-		l, err := jsonpointer.ListPointers(d)
-		if err != nil {
-			log.Fatalf("Error listing pointers: %v", err)
-		}
-		for _, p := range l {
-			fmt.Println(p)
-		}
+		listPointers(d)
 	} else {
-		m, err := jsonpointer.FindMany(d, os.Args[1:])
-		if err != nil {
-			log.Fatalf("Error finding pointers: %v", err)
-		}
-		for k, v := range m {
-			b := &bytes.Buffer{}
-			json.Indent(b, v, "", "  ")
-			fmt.Printf("%v\n%s\n\n", k, b)
-		}
+		selectItems(d, os.Args[1:])
 	}
 }
